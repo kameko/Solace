4b132c980d69e7ecee96b9a88c1ce0e0ca75fdbd
Kameko Kirishiki <kameko.k@outlook.com>
2020-01-31 15:07:07 -0500

    begin services

---
+++ Source/Solace Core/Solace.Core/Services/BaseService.cs
@@ -0,0 +1,13 @@
+
+namespace Solace.Core.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    
+    public class BaseService : IService
+    {
+        
+    }
+}
---
+++ Source/Solace Core/Solace.Core/Services/IService.cs
@@ -0,0 +1,12 @@
+
+namespace Solace.Core.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    
+    public interface IService
+    {
+        
+    }
+}
