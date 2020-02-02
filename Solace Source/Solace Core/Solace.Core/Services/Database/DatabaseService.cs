1b7454651798579f888da8b81539ac2c2eed615b
Kameko Kirishiki <kameko.k@outlook.com>
2020-02-02 11:31:31 -0500

    working on database interface

--- Solace Source/Solace Core/Solace.Core/Services/Database/DatabaseService.cs
+++ Solace Source/Solace Core/Solace.Core/Services/Database/DatabaseService.cs
@@ -5,9 +5,28 @@ namespace Solace.Core.Services.Database
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
     
     public abstract class DatabaseService : BaseService, IDatabaseService
     {
+        public virtual Task Save<T>(T item) where T : class
+        {
+            return Task.CompletedTask;
+        }
         
+        public virtual Task SaveMany<T>(IEnumerable<T> items) where T : class
+        {
+            return Task.CompletedTask;
+        }
+        
+        public virtual Task<IEnumerable<T>> Query<T>(Predicate<T> predicate) where T : class
+        {
+            return (Task.FromResult(new List<T>()) as Task<IEnumerable<T>>)!;
+        }
+        
+        public virtual Task<IEnumerable<T>> Query<T>(DbSet<T> db) where T : class
+        {
+            return (Task.FromResult(new List<T>()) as Task<IEnumerable<T>>)!;
+        }
     }
 }
