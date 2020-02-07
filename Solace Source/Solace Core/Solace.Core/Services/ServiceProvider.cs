26d35d573b0fc46e903e8ab9de618f041b00f2c4
Kameko Kirishiki <kameko.k@outlook.com>
2020-02-07 01:15:22 -0500

    service provider

--- Solace Source/Solace Core/Solace.Core/Services/ServiceProvider.cs
+++ Solace Source/Solace Core/Solace.Core/Services/ServiceProvider.cs
@@ -8,6 +8,76 @@ namespace Solace.Core.Services
     
     public class ServiceProvider
     {
+        public event Action<IService> OnServiceLoad;
+        public event Action<string> OnServiceUnload;
         
+        private List<IService> Services { get; set; }
+        private readonly object ServicesLock = new object();
+        
+        public ServiceProvider()
+        {
+            OnServiceLoad   = delegate { };
+            OnServiceUnload = delegate { };
+            Services        = new List<IService>();
+        }
+        
+        internal void AddService(IService service)
+        {
+            lock (ServicesLock)
+            {
+                Services.Add(service);
+            }
+        }
+        
+        public bool GetService(string service_name, out IService service)
+        {
+            lock (ServicesLock)
+            {
+                service = Services.Find(x => x.Name.Equals(service_name, StringComparison.InvariantCultureIgnoreCase))!;
+            }
+            
+            if (service is null)
+            {
+                return false;
+            }
+            
+            return true;
+        }
+        
+        public bool GetService<T>(string service_name, out T service) where T : IService
+        {
+            IService? temp_service = null;
+            lock (ServicesLock)
+            {
+                temp_service = Services.Find(x => x.Name.Equals(service_name, StringComparison.InvariantCultureIgnoreCase))!;
+            }
+            
+            if (!(temp_service is null) && temp_service is T tserv)
+            {
+                service = tserv;
+                return true;
+            }
+            
+            service = default!;
+            return false;
+        }
+        
+        public bool GetService<T>(out T service) where T : IService
+        {
+            IService? temp_service = null;
+            lock (ServicesLock)
+            {
+                temp_service = Services.Find(x => x is T)!;
+            }
+            
+            if (!(temp_service is null) && temp_service is T tserv)
+            {
+                service = tserv;
+                return true;
+            }
+            
+            service = default!;
+            return false;
+        }
     }
 }
