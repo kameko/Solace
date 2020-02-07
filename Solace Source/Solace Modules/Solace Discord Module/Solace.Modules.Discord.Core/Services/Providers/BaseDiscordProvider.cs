b9216775c2d47041f28c3361c5d4e6576fbb1958
Kameko Kirishiki <kameko.k@outlook.com>
2020-02-06 22:10:45 -0500

    rename to base

--- Solace Source/Solace Modules/Solace Discord Module/Solace.Modules.Discord.Core/Services/Providers/BaseDiscordProvider.cs
+++ Solace Source/Solace Modules/Solace Discord Module/Solace.Modules.Discord.Core/Services/Providers/BaseDiscordProvider.cs
@@ -6,11 +6,11 @@ namespace Solace.Modules.Discord.Core.Services.Providers
     using System.Linq;
     using System.Threading.Tasks;
     
-    public abstract class DiscordProvider : BaseProvider, IDiscordProvider
+    public abstract class BaseDiscordProvider : BaseProvider, IDiscordProvider
     {
         public event Func<DiscordMessage, Task> OnReceiveMessage;
         
-        public DiscordProvider() : base()
+        public BaseDiscordProvider() : base()
         {
             OnReceiveMessage = delegate { return Task.CompletedTask; };
         }
