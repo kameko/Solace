4344cfe305bce40c9e212f62a45cbf8d9201fef1
Kameko Kirishiki <kameko.k@outlook.com>
2020-02-10 00:37:05 -0500

    config additions

--- Solace Source/Solace Modules/Solace Discord Module/Solace.Modules.Discord.Core/Services/Providers/ProviderConfig.cs
+++ Solace Source/Solace Modules/Solace Discord Module/Solace.Modules.Discord.Core/Services/Providers/ProviderConfig.cs
@@ -11,10 +11,14 @@ namespace Solace.Modules.Discord.Core.Services.Providers
     {
         public string ConnectionToken { get; set; }
         public bool DebugLog { get; set; }
+        public int PingTimeoutMilliseconds { get; set; }
+        public int PingTries { get; set; }
         
         public ProviderConfig()
         {
             ConnectionToken = string.Empty;
+            PingTimeoutMilliseconds = 5000;
+            PingTries = 3;
         }
         
         public string? GetValue(string key)
