2db0d83e017a17a324ce93c0c04a6a95ba073e5e
Kameko Kirishiki <kameko.k@outlook.com>
2020-02-09 23:31:57 -0500

    remove locks

--- Solace Source/Solace Core/Solace.Core/Services/ServiceProvider.cs
+++ Solace Source/Solace Core/Solace.Core/Services/ServiceProvider.cs
@@ -3,40 +3,33 @@ namespace Solace.Core.Services
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.Concurrent;
     using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     
     public class ServiceProvider
     {
         public event Action<IService> OnServiceLoad;
         public event Action<string> OnServiceUnload;
-        
-        private List<IService> Services { get; set; }
-        // TODO: replace lock with something else, either a concurrent
-        // collection or SemaphoreSlim.WaitAsync()
-        private readonly object ServicesLock = new object();
+        private ConcurrentBag<IService> Services { get; set; }
         
         public ServiceProvider()
         {
             OnServiceLoad   = delegate { };
             OnServiceUnload = delegate { };
-            Services        = new List<IService>();
+            Services        = new ConcurrentBag<IService>();
         }
         
         internal void AddService(IService service)
         {
-            lock (ServicesLock)
-            {
-                Services.Add(service);
-            }
+            Services.Add(service);
         }
         
         public bool GetService(string service_name, out IService service)
         {
-            lock (ServicesLock)
-            {
-                service = Services.Find(x => x.Name.Equals(service_name, StringComparison.InvariantCultureIgnoreCase))!;
-            }
+            var ls = new List<IService>(Services);
+            service = ls.Find(x => x.Name.Equals(service_name, StringComparison.InvariantCultureIgnoreCase))!;
             
             if (service is null)
             {
@@ -49,10 +42,8 @@ namespace Solace.Core.Services
         public bool GetService<T>(string service_name, out T service) where T : IService
         {
             IService? temp_service = null;
-            lock (ServicesLock)
-            {
-                temp_service = Services.Find(x => x.Name.Equals(service_name, StringComparison.InvariantCultureIgnoreCase))!;
-            }
+            var ls = new List<IService>(Services);
+            temp_service = ls.Find(x => x.Name.Equals(service_name, StringComparison.InvariantCultureIgnoreCase))!;
             
             if (!(temp_service is null) && temp_service is T tserv)
             {
@@ -67,10 +58,8 @@ namespace Solace.Core.Services
         public bool GetService<T>(out T service) where T : IService
         {
             IService? temp_service = null;
-            lock (ServicesLock)
-            {
-                temp_service = Services.Find(x => x is T)!;
-            }
+            var ls = new List<IService>(Services);
+            temp_service = ls.Find(x => x is T)!;
             
             if (!(temp_service is null) && temp_service is T tserv)
             {
