eb40f9399c3627461ac419d77aa4bb0e011bf4a5
Kameko Kirishiki <kameko.k@outlook.com>
2020-02-12 01:53:44 -0500

    core base test

---
+++ Solace Source/Solace Core/Solace.Tests/BaseTest.cs
@@ -0,0 +1,23 @@
+
+namespace Solace.Tests
+{
+    using System;
+    using Xunit.Abstractions;
+    
+    public class BaseTest
+    {
+        private readonly ITestOutputHelper output;
+        
+        public BaseTest(ITestOutputHelper output)
+        {
+            this.output = output;
+            Write(string.Empty);
+        }
+        
+        protected void Write(string message)
+        {
+            output.WriteLine(message);
+            Console.WriteLine(message);
+        }
+    }
+}
