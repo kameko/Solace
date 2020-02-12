6d676d8af717dd42ae17497ca9a7ae476661e2ef
Kameko Kirishiki <kameko.k@outlook.com>
2020-02-12 01:26:22 -0500

    begin unit test

---
+++ Solace Source/Solace Modules/Solace Discord Module/Solace.Modules.Discord.Tests/Core/Services/Providers/DiscordChannelQueryTokenTests.cs
@@ -0,0 +1,16 @@
+
+namespace Solace.Modules.Discord.Tests.Core.Services.Providers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Xunit;
+    using Xunit.Abstractions;
+    using Core.Services.Providers;
+    
+    public class DiscordChannelQueryTokenTests
+    {
+        
+    }
+}
