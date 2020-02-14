38a41e44dade23692120417e41d6cf0bfb13e16d
Kameko Kirishiki <kameko.k@outlook.com>
2020-02-14 17:08:51 -0500

    module work

--- Solace Source/Solace Core/Solace.Core/Modules/ModuleContainer.cs
+++ Solace Source/Solace Core/Solace.Core/Modules/ModuleContainer.cs
@@ -4,30 +4,68 @@ namespace Solace.Core.Modules
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     
-    public class ModuleContainer
+    public class ModuleContainer : IDisposable
     {
+        public string Name { get; set; }
+        private CancellationTokenSource? Token { get; set; }
         private ModuleLoader? Loader { get; set; }
         private BaseModule? Module { get; set; }
         
-        public ModuleContainer()
+        public ModuleContainer(string name)
         {
-            
+            Name = name;
         }
         
-        public BaseModule Load(string name, string path)
+        public BaseModule Load(string path)
         {
-            Loader = new ModuleLoader(name);
-            return Loader.LoadModule(path);
+            Loader = new ModuleLoader(Name);
+            Module = Loader.LoadModule(path);
+            return Module;
         }
         
-        public bool TryLoad(string name, string path, out BaseModule? module)
+        public bool TryLoad(string path, out BaseModule? module)
         {
-            Loader = new ModuleLoader(name);
-            return Loader.TryLoadModule(path, out module);
+            Loader = new ModuleLoader(Name);
+            var success = Loader.TryLoadModule(path, out module);
+            Module = module;
+            return success;
         }
         
+        public void Run()
+        {
+            Token = new CancellationTokenSource();
+            var services = Module?.GetServices();
+            foreach (var service in services!)
+            {
+                service.Start(Token.Token);
+            }
+        }
         
+        public void Stop()
+        {
+            Token?.Cancel();
+        }
+        
+        public bool TryLoad(string path)
+        {
+            return TryLoad(path, out var _);
+        }
+        
+        public void Unload()
+        {
+            Stop();
+            Module = null;
+            Loader?.Dispose();
+            Loader = null;
+            GC.Collect();
+        }
+        
+        public void Dispose()
+        {
+            Unload();
+        }
     }
 }
--- Solace Source/Solace Core/Solace.Core/Modules/ModuleManager.cs
+++ Solace Source/Solace Core/Solace.Core/Modules/ModuleManager.cs
@@ -5,6 +5,7 @@ namespace Solace.Core.Modules
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Services;
     
     public class ModuleManager
     {
@@ -13,6 +14,45 @@ namespace Solace.Core.Modules
         public ModuleManager()
         {
             Containers = new List<ModuleContainer>();
+        }
+        
+        public IEnumerable<IService> Load(string name, string path)
+        {
+            var container = new ModuleContainer(name);
+            try
+            {
+                var module = container.Load(name);
+                var services = module.GetServices();
+                Containers.Add(container);
+                
+                // TODO: handle dependencies.
+                
+                container.Run();
+                return services;
+            }
+            catch
+            {
+                // TODO: log failure
+                // TODO: some sort of OnFailure callback
+                return new List<IService>();
+            }
+        }
+        
+        public void Unload(string name)
+        {
+            
+        }
+        
+        private class DependencyQueueToken
+        {
+            public ModuleContainer Container { get; set; }
+            public List<string> DependenciesLeft { get; set; }
+            
+            public DependencyQueueToken(ModuleContainer container, List<string> dependencies)
+            {
+                Container        = container;
+                DependenciesLeft = dependencies;
+            }
         }
     }
 }
