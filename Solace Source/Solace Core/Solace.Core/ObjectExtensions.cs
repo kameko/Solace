d36d1b11a7efea294b0c4d606a161cd05383f142
Kameko Kirishiki <kameko.k@outlook.com>
2020-02-13 19:53:39 -0500

    automatic object difference detection

---
+++ Solace Source/Solace Core/Solace.Core/ObjectExtensions.cs
@@ -0,0 +1,35 @@
+
+namespace Solace.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    
+    public static class ObjectExtensions
+    {
+        public static string GetObjectDifferencesAsString(this object before, object after)
+        {
+            var diff = string.Empty;
+            var before_props = before.GetType().GetProperties();
+            var after_props = before.GetType().GetProperties();
+            for (var i = 0; i < before_props.Count(); i++)
+            {
+                var before_prop = before_props.ElementAt(i);
+                var before_prop_value = before_prop.GetValue(before, before_prop.GetIndexParameters());
+                var after_prop = after_props.ElementAt(i);
+                var after_prop_value = after_prop.GetValue(after, after_prop.GetIndexParameters());
+                if (!before_prop_value!.Equals(after_prop_value))
+                {
+                    var prop_name = before_prop.Name;
+                    if (!string.IsNullOrEmpty(diff))
+                    {
+                        diff += ", ";
+                    }
+                    diff += $"{prop_name}: {before_prop_value} -> {after_prop_value}";
+                }
+            }
+            return diff;
+        }
+    }
+}
