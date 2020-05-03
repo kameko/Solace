b74ee5e648aaab1a4f06ddaaea946897b2371ab8
Kameko Kirishiki <kameko.k@outlook.com>
2020-05-03 13:54:10 -0400

    rename

--- Source/Core/Caesura.Solace.Manager/Controllers/ProcController.cs
+++ Source/Core/Caesura.Solace.Manager/Controllers/ProcController.cs
@@ -2,9 +2,6 @@
 namespace Caesura.Solace.Manager.Controllers
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
     using Foundation.ApiBoundaries;
